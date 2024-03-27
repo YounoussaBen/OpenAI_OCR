dd29dbd7424ff1e6695f42bade4c20bebb912ba8
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-03-27 16:11:25 +0000

    main from openai docs

---
+++ main.py
@@ -0,0 +1,51 @@
+import base64
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Get OpenAI API Key from environment variable
+api_key = os.getenv("OPENAI_API_KEY")
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "images/menu.png"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4-vision-preview",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What’s in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
